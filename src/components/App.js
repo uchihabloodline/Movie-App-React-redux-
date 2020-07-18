7d44fd27872b49e2bf95202d8fb2a51e850bdeac
shivam <geetapandey595@gmail.com>
2020-07-18 23:37:27 +0530

    added redux part

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Navbar from './navbar';
+import MovieCard from './MovieCard';
+
+import { data } from '../data';
 
 function App() {
   return (
     <div className="App">
-      Project setup
+      <Navbar />
+      <div className="main">
+        <div className="tabs">Movies</div>
+        <div className="tabs">Favourites</div>
+      </div>
+
+      <div className="list">
+        {data.map((movie,index)=>(                        //function writing doubt
+          <MovieCard movie = {movie} key={`movies ${index}`} />
+        ))}
+
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//imported redux store
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers.js';
+
+//added redux store object below
+const store = createStore(movies);
+console.log('store ',store);
+console.log('STATE ',store.getState());   //to fetch and check the current state of imported reducer.
 
 ReactDOM.render(
   <React.StrictMode>
